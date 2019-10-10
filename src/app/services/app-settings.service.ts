71d601665c2ffb5a1d6a430ec417bbe274ff7f9c
nader.hatami <nader.hatami@slalom.com>
2019-10-10 19:05:08 -0400

    calling api to fetch news categories and countries instead of reading from static files locally

---
+++ src/app/services/app-settings.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppSettingsService {
+  constructor(private http: HttpClient) {}
+
+  public getCountries(): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/news/countries`);
+  }
+
+  public getNewsCategories(): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/news/categories`);
+  }
+}
